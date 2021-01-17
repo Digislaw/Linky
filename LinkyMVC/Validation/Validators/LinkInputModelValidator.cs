2f834512819203eb2343c63f1908ab4eeef81e21
Digislaw <digi2978@gmail.com>
2021-01-17 12:18:58 +0100

    Sprawdzenie czy etykieta linku zawiera jedynie znaki alfanumeryczne

--- LinkyMVC/Validation/Validators/LinkInputModelValidator.cs
+++ LinkyMVC/Validation/Validators/LinkInputModelValidator.cs
@@ -8,7 +8,9 @@ namespace LinkyMVC.Validation.Validators
         public LinkInputModelValidator()
         {
             RuleFor(l => l.Label).NotEmpty().MaximumLength(50);
-            
+            RuleFor(l => l.Label).Matches(@"^[a-zA-Z0-9]+$")
+                .WithMessage("Label should contain letters and digits only");
+
             RuleFor(l => l.URL).NotEmpty().MaximumLength(256)
                 .Must(CustomRules.ValidURL).WithMessage("URL must be valid");
         }
--- LinkyMVC/Validation/Validators/LinkUpdateModelValidator.cs
+++ LinkyMVC/Validation/Validators/LinkUpdateModelValidator.cs
@@ -8,6 +8,8 @@ namespace LinkyMVC.Validation.Validators
         public LinkUpdateModelValidator()
         {
             RuleFor(l => l.Label).NotEmpty().MaximumLength(50);
+            RuleFor(l => l.Label).Matches(@"^[a-zA-Z0-9]+$")
+                .WithMessage("Label should contain letters and digits only");
 
             RuleFor(l => l.URL).NotEmpty().MaximumLength(256)
                 .Must(CustomRules.ValidURL).WithMessage("URL must be valid");
